1a0125c2a389bf7ed8a86982a25d400cdddae1cc
yasar2385 <yasar.mohideen@nkw.pub>
2022-08-10 19:11:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vecqrh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vecqrh)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+import Chart from "chart.js";
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
